b506e8917e9943670f1db8a63e80d883478ffc0a
acepnurmansidik <acepnurmansidik@gmail.com>
2021-12-30 07:04:11 +0700

    Film | Successfuly Update & Delete, fix detail

--- app/Film/controller.js
+++ app/Film/controller.js
@@ -68,4 +68,63 @@ module.exports = {
       console.log(err.message);
     }
   },
+  viewEditFilm: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      let film = await Film.findOne({ _id: id }).populate("genres");
+      let genres = await Genre.find();
+
+      res.render("admin/Film/editFilm", {
+        title: `Admin | Update Film`,
+        film,
+        genres,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  actionEditFilm: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      const {
+        title,
+        actor,
+        synopsis,
+        director,
+        writter,
+        rating,
+        genres,
+        category,
+      } = req.body;
+
+      await Film.findOneAndUpdate(
+        { _id: id },
+        {
+          title,
+          actor,
+          synopsis,
+          director,
+          writter,
+          rating,
+          genres,
+          category,
+        }
+      );
+
+      res.redirect("/film");
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  actionDeleteFilm: async (req, res) => {
+    try {
+      const { id } = req.params;
+      await Film.findOneAndRemove({ _id: id });
+      res.redirect("/film");
+    } catch (err) {
+      console.log(err);
+    }
+  },
 };
--- app/Film/router.js
+++ app/Film/router.js
@@ -4,6 +4,9 @@ const {
   actionAddFilm,
   viewAddFilm,
   viewDetailFilm,
+  viewEditFilm,
+  actionEditFilm,
+  actionDeleteFilm,
 } = require("./controller");
 const router = express.Router();
 
@@ -11,8 +14,13 @@ const router = express.Router();
 router.get("/film", viewFilm);
 // detail
 router.get("/film/detail/:id", viewDetailFilm);
-// update
+// create
 router.get("/film/create", viewAddFilm);
 router.post("/film/create", actionAddFilm);
+// update
+router.get("/film/update/:id", viewEditFilm);
+router.put("/film/update/:id", actionEditFilm);
+// delete
+router.delete("/film/delete/:id", actionDeleteFilm);
 
 module.exports = router;
